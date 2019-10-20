ce995133ff7a6df36063ef5a0900a798bfaeb4c1
Blake Johnston <johnstonbl01@gmail.com>
2019-10-20 16:59:03 -0500

    Handle form errors

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,11 +1,20 @@
 import { useState, useContext } from 'preact/hooks';
+import startCase from 'lodash/startCase';
 import ContentLayout from '../ContentLayout/ContentLayout';
+import Icon from '../Icon/Icon';
 import TextInput from './TextInput/TextInput';
 import styles from './Register.css';
 import CheckBox from './CheckBox/CheckBox';
 import Rating from '../Rating/Rating';
 import icons from '../../theme/icons';
 import FirebaseContext from '../../context/Firebase';
+
+const ErrorText = ({ error }) => (
+  <div class={styles.errorLayout}>
+    <h3 class={styles.error}>{error}</h3>
+    <Icon icon={icons.sadface} width={24} viewBox="0 0 496 512" color="#ff6a75" />
+  </div>
+);
 
 const Register = () => {
   const db = useContext(FirebaseContext).firestore();
@@ -14,41 +23,48 @@ const Register = () => {
   const [chiliName, setChiliName] = useState('');
   const [isVeg, setIsVeg] = useState(false);
   const [spiceLevel, setSpiceLevel] = useState(1);
+  const [error, setError] = useState('');
 
   const onSubmit = evt => {
     evt.preventDefault();
     setSubmitting(true);
 
-    if (validateForm(name, chiliName, isVeg, spiceLevel)) {
-      return db
-        .collection('entrants')
-        .add({
-          name,
-          chiliName,
-          isVeg,
-          spiceLevel,
-          createdTs: Date.now()
-        })
-        .then(() => {
-          setSubmitting(false);
-          setName('');
-          setChiliName('');
-          setIsVeg(false);
-          setSpiceLevel(1);
+    const formIsValid = validateForm(name, chiliName, isVeg, spiceLevel);
 
-          console.info('Successfully added to Firebase');
-        })
-        .catch(err => {
-          console.error('Error when submitting data to Firebase', err);
-
-          setSubmitting(false);
-          // handle error
-        });
+    if (!formIsValid) {
+      setError('Stop trying to be fancy, Steve');
     }
+
+    return db
+      .collection('entrants')
+      .add({
+        name: startCase(name),
+        chiliName: startCase(chiliName),
+        isVeg,
+        spiceLevel,
+        createdTs: Date.now()
+      })
+      .then(() => {
+        setSubmitting(false);
+        setName('');
+        setChiliName('');
+        setIsVeg(false);
+        setSpiceLevel(1);
+        setError('');
+
+        console.info('Successfully added to Firebase');
+      })
+      .catch(err => {
+        setError('The hamsters have failed us');
+        setSubmitting(false);
+
+        console.error('Error when submitting data to Firebase', err);
+      });
   };
 
   return (
     <ContentLayout title="Register">
+      {error && <ErrorText error={error} />}
       <form onSubmit={onSubmit} class={styles.form}>
         <TextInput
           placeholder="Your Name"
@@ -83,7 +99,7 @@ function validateForm(...values) {
 
     switch (type) {
       case 'string':
-        return value.length > 0;
+        return value.length > 0 && value.length < 256;
       case 'number':
         return value > 0 && value <= 5;
       case 'boolean':
