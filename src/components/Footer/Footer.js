ff49c84ead10a057c216ba96d6c8d7dd3eb4ca84
Blake Johnston <johnstonbl01@gmail.com>
2019-10-26 14:01:50 -0500

    Add footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,8 @@
+import styles from './Footer.css';
+
 const Footer = () => (
-  <footer>
-    <p>Copyright me 2019</p>
+  <footer class={styles.footer}>
+    <p class={styles.copyright}>&copy; 2019 Blake Johnston</p>
   </footer>
 );
 
